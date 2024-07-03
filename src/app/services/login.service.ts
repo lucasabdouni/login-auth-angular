7f25edbc2378589dd4fa9abfd6a069e322e4ce72
lucasabdouni@hotmail.com <lucasabdouni@hotmail.com>
2024-07-03 16:08:25 -0300

    feat: implements fetch api and guard for route authenticated

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,11 +7,21 @@ import { LoginResponse } from '../types/login-response.type';
   providedIn: 'root'
 })
 export class LoginService {
+  apiUrl: string = 'http://localhost:8080/auth';
 
   constructor(private httpClient: HttpClient) { }
 
-  login(name: string, password: string) {
-    return this.httpClient.post<LoginResponse>('/login', { name, password }).pipe(
+  login(email: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+      sessionStorage.setItem("auth-token", value.token)
+      sessionStorage.setItem("username", value.name)
+    })
+    )
+  }
+
+  signup(name: string, email: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
       tap((value) => {
       sessionStorage.setItem("auth-token", value.token)
       sessionStorage.setItem("username", value.name)
